eb877987c2df497b8524499f116b7f4b313e6369
Hans van der Woude <2658411+nedix@users.noreply.github.com>
2026-03-04 22:59:05 +0100

    Fix SECRET_KEY special characters

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -3,12 +3,12 @@
 : ${IS_PUBLIC_INSTANCE:="false"}
 : ${SECRET_KEY}
 
-yq -i ".server.secret_key = ${SECRET_KEY}" /etc/searxng/settings.yml
+yq -i ".server.secret_key = env(SECRET_KEY)" /etc/searxng/settings.yml
 
 unset SECRET_KEY
 
 yq -i ".engines[].disabled = true" /usr/local/searxng/searx/settings.yml
-yq -i ".server.public_instance = ${IS_PUBLIC_INSTANCE}" /etc/searxng/settings.yml
+yq -i ".server.public_instance = env(IS_PUBLIC_INSTANCE)" /etc/searxng/settings.yml
 
 cd /usr/local/searxng/
 
