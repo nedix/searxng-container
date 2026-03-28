7ce3a7bb38df6dcc7370b938d523b8464480f659
Hans van der Woude <2658411+nedix@users.noreply.github.com>
2026-03-28 19:51:50 +0100

    Add donation page

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -2,9 +2,15 @@
 
 : ${CATEGORIES}
 : ${DEFAULT_ENGINES}
-: ${ENABLE_METRICS:="false"}
+: ${ENABLE_DONATION_URL:="true"}
+: ${ENABLE_METRICS:="true"}
 : ${IS_PUBLIC_INSTANCE:="true"}
 : ${SECRET_KEY}
+
+if [ -z "$SECRET_KEY" ]; then
+    echo "The SECRET_KEY variable must be defined."
+    exit 1
+fi
 
 # -------------------------------------------------------------------------------
 #    Bootstrap searxng services
@@ -15,11 +21,12 @@
     # -------------------------------------------------------------------------------
     mkdir -p /run/searxng-configure/environment
 
-    echo "$CATEGORIES"         > /run/searxng-configure/environment/CATEGORIES
-    echo "$DEFAULT_ENGINES"    > /run/searxng-configure/environment/DEFAULT_ENGINES
-    echo "$ENABLE_METRICS"     > /run/searxng-configure/environment/ENABLE_METRICS
-    echo "$IS_PUBLIC_INSTANCE" > /run/searxng-configure/environment/IS_PUBLIC_INSTANCE
-    echo "$SECRET_KEY"         > /run/searxng-configure/environment/SECRET_KEY
+    echo "$CATEGORIES"          > /run/searxng-configure/environment/CATEGORIES
+    echo "$DEFAULT_ENGINES"     > /run/searxng-configure/environment/DEFAULT_ENGINES
+    echo "$ENABLE_DONATION_URL" > /run/searxng-configure/environment/ENABLE_DONATION_URL
+    echo "$ENABLE_METRICS"      > /run/searxng-configure/environment/ENABLE_METRICS
+    echo "$IS_PUBLIC_INSTANCE"  > /run/searxng-configure/environment/IS_PUBLIC_INSTANCE
+    echo "$SECRET_KEY"          > /run/searxng-configure/environment/SECRET_KEY
 }
 
 # -------------------------------------------------------------------------------
---
+++ rootfs/usr/local/searxng/searx/infopage/en/donate.md
@@ -0,0 +1,16 @@
+# Make a donation
+
+This instance of SearXNG is powered by the [nedix/searxng (Docker Hub)](https://hub.docker.com/r/nedix/searxng) container. Unlike the standard [searxng/searxng (Docker Hub)](https://hub.docker.com/r/searxng/searxng) container the following additional features are included:
+
+- bot detection and mitigation
+- configuration through environment variables
+- whitelist approach to engine and plugin configuration
+
+## Ways to support
+
+You can donate using one of these methods:
+
+- **Bitcoin (BTC):** `bc1q0g0uuwwt8y40nfj27pxxs3eqp6y87k2y0k5vme`
+- **PayPal:** [paypal.me/nedix](https://paypal.me/nedix)
+
+Every donation supports maintenance and improvements to keep the container secure and up-to-date. Thank you for your support!
